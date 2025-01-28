c89be1d614e706466dc5c95a856ecd5249298b0f
liahychka <157300297+liahychka@users.noreply.github.com>
2025-01-28 14:36:04 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from 'react';
 import Navigation from './components/Navigation/Navigation';
 
+<<<<<<< Updated upstream
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const Catalog = lazy(() => import('./pages/Catalog/Catalog'));
 const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
 const CampersDetailsPage = lazy(() => import('./pages/CampersDetailsPage/CampersDetailsPage'));
 const ReviewsCampers = lazy(() => import('./components/ReviewsCampers/ReviewsCampers'));
 const FeaturesCampers = lazy(() => import('./components/FeaturesCampers/FeaturesCampers'));
+=======
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
+const MovieDetails = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -16,10 +26,17 @@ const App = () => {
       <Suspense fallback={ <h2>Loading by suspense</h2> } >
                   <Routes>
         <Route path="/" element={<HomePage />}/>
+<<<<<<< Updated upstream
         <Route path="/catalog" element={<Catalog />} />
         <Route path="catalog/:id" element={<CampersDetailsPage />} >
           <Route path="features" element={<FeaturesCampers />} />
           <Route path="reviews" element={<ReviewsCampers />}/>
+=======
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="movies/:movieId" element={<MovieDetails />} >
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />}/>
+>>>>>>> Stashed changes
         </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // import React, { useEffect, useState } from 'react'
 // import MovieList from '../../components/MovieList/MovieList'
 // import { fetchMovie } from '../../services/api';
@@ -51,4 +52,43 @@ const HomePage = () => {
     </div>
   )
 }
+=======
+import React, { useEffect, useState } from 'react'
+import MovieList from '../../components/MovieList/MovieList'
+import { fetchMovie } from '../../services/api';
+
+const HomePage = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const getAllMovies = async () => {
+      setIsLoading(true);
+      setError(null)
+      try {
+        const data = await fetchMovie();
+        setMovies(data.results); 
+      } catch (error) {
+        setError(error)
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getAllMovies();
+  }, []);
+
+    return (
+      <div>
+        <h2>Trending today</h2>
+        {isLoading && <p>Loading</p>}
+        {error && <p>404</p>}
+        {movies.length > 0 && <MovieList movies={movies} /> }
+      </div>
+
+  )
+}
+
+>>>>>>> Stashed changes
 export default HomePage
